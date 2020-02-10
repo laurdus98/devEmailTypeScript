8c9f3ebb6e7759f897f045e0c2c39db36600ee1c
Loris Loreto Santaroni <lsantaroni@key-biz.it>
2020-02-10 10:27:37 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div style={{ backgroundImage: logo, minWidth: '100vw', minHeight: '100vh' }} />
+			</header>
+		</div>
+	);
+}
