5db4b986bb983997f6f78ef2d2696934c3af39ba
Loris Loreto Santaroni <lsantaroni@key-biz.it>
2020-03-05 18:29:46 +0100

    modify: changeAll

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,93 @@
-import React, { FC, useState, useEffect } from 'react';
-import './App.css';
-import { DevMailComponent } from './devEmail/view/DevMailComponent';
-import { connect } from 'react-redux';
-import { mapState } from './devEmail/services/mapState';
-import { mapDispatch } from './devEmail/services/mapDispatch';
-import { IProps } from './devEmail/interfaces/IProps';
-import { IGettingSuccessAction, IGettingFailureAction } from './devEmail/redux/actions/actions';
-import { Mail } from './devEmail/model/Mail';
-import { Categoria } from './devEmail/model/Categoria';
-import { Messaggio } from './devEmail/model/Messaggio';
+import React, { FC, useState, useEffect } from "react";
+import "./App.css";
+import { DevMailComponent } from "./devEmail/view/DevMailComponent";
+import { connect } from "react-redux";
+import { mapState } from "./devEmail/services/mapState";
+import { mapDispatch } from "./devEmail/services/mapDispatch";
+import { IProps } from "./devEmail/interfaces/IProps";
+import {
+  IGettingSuccessAction,
+  IGettingFailureAction
+} from "./devEmail/redux/actions/actions";
+import { Mail } from "./devEmail/model/Mail";
+import { Categoria } from "./devEmail/model/Categoria";
+import { Messaggio } from "./devEmail/model/Messaggio";
+import { MailAPI } from "./devEmail/apis/fakeAPI/MailAPI";
+import { MessaggiAPI } from "./devEmail/apis/fakeAPI/MessaggiAPI";
+import { CategoriaAPI } from "./devEmail/apis/fakeAPI/CategoriaAPI";
 
-const App: FC<IProps> = ({ fetchingMail, fetchingMessage, fetchingCategoria, logo }) => {
-	const [
-		isFetch,
-		setFetch
-	] = useState(false);
+const App: FC<IProps> = ({
+  fetchingMail,
+  fetchingMessage,
+  fetchingCategoria,
+  logo
+}) => {
+  const [isFetch, setFetch] = useState(false);
 
-	const [
-		payloadMail,
-		setPayloadMail
-	] = useState<Mail[]>([]);
+  const [payloadMail, setPayloadMail] = useState<Mail[]>([]);
 
-	const [
-		payloadMessaggi,
-		setPayloadMessaggi
-	] = useState<Messaggio[]>([]);
+  const [payloadMessaggi, setPayloadMessaggi] = useState<Messaggio[]>([]);
 
-	const [
-		payloadCategoria,
-		setPayloadCategoria
-	] = useState<Categoria[]>([]);
+  const [payloadCategoria, setPayloadCategoria] = useState<Categoria[]>([]);
 
-	useEffect(
-		() => {
-			if (!isFetch) {
-				fetchingMail('/Mail')
-					.then((resp: IGettingSuccessAction<Mail>) => {
-						const { payload, status, type }: IGettingSuccessAction<Mail> = resp;
-						if (status === 'OK' && type === 'Success') {
-							setPayloadMail(payload);
-						}
-					})
-					.catch((error: IGettingFailureAction) => console.log(error));
-				fetchingMessage('/Messaggi')
-					.then((resp: IGettingSuccessAction<Messaggio>) => {
-						const { payload, status, type }: IGettingSuccessAction<Messaggio> = resp;
-						if (status === 'OK' && type === 'Success') {
-							setPayloadMessaggi(payload);
-						}
-					})
-					.catch((error: IGettingFailureAction) => console.log(error));
-				fetchingCategoria('/Categoria')
-					.then((resp: IGettingSuccessAction<Categoria>) => {
-						const { payload, status, type }: IGettingSuccessAction<Categoria> = resp;
-						if (status === 'OK' && type === 'Success') {
-							setPayloadCategoria(payload);
-						}
-					})
-					.catch((error: IGettingFailureAction) => console.log(error));
-				setFetch(true);
-			}
-		},
-		[
-			isFetch,
-			setFetch,
-			fetchingMail,
-			fetchingMessage,
-			fetchingCategoria
-		]
-	);
+  useEffect(() => {
+    if (!isFetch) {
+      fetchingMail("/Mail")
+        .then((resp: IGettingSuccessAction<Mail>) => {
+          const { payload, status, type }: IGettingSuccessAction<Mail> = resp;
+          if (status === "OK" && type === "Success") {
+            setPayloadMail(payload);
+          } else {
+            setPayloadMail(MailAPI.mails);
+          }
+        })
+        .catch((error: IGettingFailureAction) => console.log(error));
+      fetchingMessage("/Messaggi")
+        .then((resp: IGettingSuccessAction<Messaggio>) => {
+          const {
+            payload,
+            status,
+            type
+          }: IGettingSuccessAction<Messaggio> = resp;
+          if (status === "OK" && type === "Success") {
+            setPayloadMessaggi(payload);
+          } else {
+            setPayloadMessaggi(MessaggiAPI.messaggi);
+          }
+        })
+        .catch((error: IGettingFailureAction) => console.log(error));
+      fetchingCategoria("/Categoria")
+        .then((resp: IGettingSuccessAction<Categoria>) => {
+          const {
+            payload,
+            status,
+            type
+          }: IGettingSuccessAction<Categoria> = resp;
+          if (status === "OK" && type === "Success") {
+            setPayloadCategoria(payload);
+          } else {
+            setPayloadCategoria(CategoriaAPI.categorie);
+          }
+        })
+        .catch((error: IGettingFailureAction) => console.log(error));
+      setFetch(true);
+    }
+  }, [isFetch, setFetch, fetchingMail, fetchingMessage, fetchingCategoria]);
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<div style={{}}>
-					<DevMailComponent
-						logo={logo}
-						mail={payloadMail}
-						messaggio={payloadMessaggi}
-						categoria={payloadCategoria}
-					/>
-				</div>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{}}>
+          <DevMailComponent
+            logo={logo}
+            mail={payloadMail}
+            messaggio={payloadMessaggi}
+            categoria={payloadCategoria}
+          />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default connect(mapState, mapDispatch)(App);
--- src/devEmail/apis/fakeAPI/CategoriaAPI.ts
+++ src/devEmail/apis/fakeAPI/CategoriaAPI.ts
@@ -1,58 +1,60 @@
+import { CategoriaEnum } from "../../model/Categoria";
+
 export const CategoriaAPI = {
-	categorie: [
-		{
-			id: 1,
-			updateAt: '2020-02-10T00:00:00',
-			updateBy: 'admin',
-			tipo: 'SocialNetwork',
-			categoria: {
-				id: 1,
-				updateAt: '2020-02-10T00:00:00',
-				updateBy: 'admin',
-				contenuto: 'Messaggio 1',
-				messaggi: {
-					id: 1,
-					updateAt: '2020-02-10T00:00:00',
-					updateBy: 'admin',
-					notifica: false
-				}
-			}
-		},
-		{
-			id: 2,
-			updateAt: '2020-02-10T00:00:00',
-			updateBy: 'admin',
-			tipo: 'Promozioni',
-			categoria: {
-				id: 2,
-				updateAt: '2020-02-10T00:00:00',
-				updateBy: 'admin',
-				contenuto: 'Messaggio 2',
-				messaggi: {
-					id: 2,
-					updateAt: '2020-02-10T00:00:00',
-					updateBy: 'admin',
-					notifica: false
-				}
-			}
-		},
-		{
-			id: 3,
-			updateAt: '2020-02-10T00:00:00',
-			updateBy: 'admin',
-			tipo: 'SocialNetwork',
-			categoria: {
-				id: 2,
-				updateAt: '2020-02-10T00:00:00',
-				updateBy: 'admin',
-				contenuto: 'Messaggio 2',
-				messaggi: {
-					id: 2,
-					updateAt: '2020-02-10T00:00:00',
-					updateBy: 'admin',
-					notifica: false
-				}
-			}
-		}
-	]
+  categorie: [
+    {
+      id: 1,
+      updateAt: "2020-02-10T00:00:00",
+      updateBy: "admin",
+      tipo: CategoriaEnum.SocialNetwork,
+      categoria: {
+        id: 1,
+        updateAt: "2020-02-10T00:00:00",
+        updateBy: "admin",
+        contenuto: "Messaggio 1",
+        messaggi: {
+          id: 1,
+          updateAt: "2020-02-10T00:00:00",
+          updateBy: "admin",
+          notifica: false
+        }
+      }
+    },
+    {
+      id: 2,
+      updateAt: "2020-02-10T00:00:00",
+      updateBy: "admin",
+      tipo: CategoriaEnum.Promozioni,
+      categoria: {
+        id: 2,
+        updateAt: "2020-02-10T00:00:00",
+        updateBy: "admin",
+        contenuto: "Messaggio 2",
+        messaggi: {
+          id: 2,
+          updateAt: "2020-02-10T00:00:00",
+          updateBy: "admin",
+          notifica: false
+        }
+      }
+    },
+    {
+      id: 3,
+      updateAt: "2020-02-10T00:00:00",
+      updateBy: "admin",
+      tipo: CategoriaEnum.SocialNetwork,
+      categoria: {
+        id: 2,
+        updateAt: "2020-02-10T00:00:00",
+        updateBy: "admin",
+        contenuto: "Messaggio 2",
+        messaggi: {
+          id: 2,
+          updateAt: "2020-02-10T00:00:00",
+          updateBy: "admin",
+          notifica: false
+        }
+      }
+    }
+  ]
 };
