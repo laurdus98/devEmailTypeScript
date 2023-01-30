d63488aa858224d322bc6acdf923689af501140c
lorissantaroni <laurdus98@gmail.com>
2023-01-30 14:46:26 +0100

    fix vulnerabilities and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import logo from './logo.svg';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App logo={logo} />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
